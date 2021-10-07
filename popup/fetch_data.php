42def28d662d3cfda8e7de622b0692200c44c89c
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-06 20:08:34 -0700

    up

--- popup/fetch_data.php
+++ popup/fetch_data.php
@@ -153,7 +153,7 @@ if($style=="Rounded"){
               ';
           }
        //echo "<table style='border:1px solid gray;border-radius:$border;' >";
-        echo "<table style='border:1px solid gray;border-radius:$border;' >";
+        echo "<table >";
        echo "<tr>";
        echo "<td>"."<img style='width:62px; height:62px;border-radius: 30px 30px;' src=".$up.">"."</td>";
        echo "<td>".
