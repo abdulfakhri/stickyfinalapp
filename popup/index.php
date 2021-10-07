1808dce06456f95b6bdcfef8d63c9df9015cc3c6
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-07 15:10:42 -0700

    up

--- popup/index.php
+++ popup/index.php
@@ -75,7 +75,7 @@ $(document).ready(function(){
  });
  
  setInterval(function(){//setInterval() method execute on every interval until called clearInterval()
-  $('#load_posts').load("fetch_data.php").fadeIn("slow");
+  $('#load_posts').load("fetch_data.php?q=8").fadeIn("slow");
   //load() method fetch data from fetch.php page
  }, 1000);
  
