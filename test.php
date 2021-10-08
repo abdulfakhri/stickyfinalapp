6ba69a6f59264c812e0ca0c4184b488b4a962c61
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-08 13:57:40 -0700

    up

--- test.php
+++ test.php
@@ -1,15 +1,1 @@
-<iframe 
-width="300" 
-height="90" 
-style="border:0px solid gray;" 
-src="http://stickyapp.regrowup.com/popup/fetch_data.php?q=6&delay=500&style=Rounded" 
-title="Javascript Code" 
-frameborder="0" 
-allow="accelerometer; 
-autoplay; 
-clipboard-write; 
-encrypted-media;
- gyroscope; 
- picture-in-picture" 
- allowfullscreen> 
-</iframe>
+<iframe width="300" height="90" style="border:1px solid gray;" src="http://stickyapp.regrowup.com/popup/app.php?u=6&delay=5&style=30px" title="Javascript Code" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>
