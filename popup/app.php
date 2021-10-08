120f09db7a86d26fe9e09dd958b3382e7977bd27
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-07 21:38:57 -0700

    up

--- popup/app.php
+++ popup/app.php
@@ -74,15 +74,6 @@ var x = setInterval(function() {
 
 var now = new Date().getTime();
 var t = deadline - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-
-document.getElementById("day").innerHTML =days ;
-document.getElementById("hour").innerHTML =hours;
-document.getElementById("minute").innerHTML = minutes;
-document.getElementById("second").innerHTML =seconds;
 
     if (t == 0) {
 		clearInterval(x);
