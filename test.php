97391f4f8b1f6ff4dd9442b4a0b4a40e5f9270db
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-07 22:03:06 -0700

    up

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <iframe width="300" height="90" style="border:0px solid gray;" 
-src="http://stickyapp.regrowup.com/popup/fetch_data.php?q=6&delay=5&style=Rounded" 
+src="http://stickyapp.regrowup.com/popup/fetch_data.php?q=6&delay=500&style=Rounded" 
 title="Javascript Code" 
 frameborder="0" 
 allow="accelerometer; 
