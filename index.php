ce52d060b9e7e08b30a491a9d40fb4e7fceec684
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-08 14:11:54 -0700

    up

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-require_once ('/includes/header.php');
-	require_once '/includes/db.php';
+require_once ('includes/header.php');
+	require_once 'includes/db.php';
 
-	require_once '/Controllers/CampaignController.php';
+	require_once 'Controllers/CampaignController.php';
 
 	$db = new DBController();
 
