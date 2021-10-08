ce672e91bf677d7a6dead05d2122d78816f7570d
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-07 21:13:12 -0700

    up

--- popup/app.php
+++ popup/app.php
@@ -66,27 +66,32 @@ smalltext{
 
 <script>
 
-var deadline = new Date("dec 31, 2017 15:37:25").getTime();
+var deadline = <?php echo date('Y-m-d h:i:s'); ?>;
+
 
 var x = setInterval(function() {
 
-var now = new Date().getTime();
+var now = <?php echo date('Y-m-d h:i:s'); ?>;
 var t = deadline - now;
 var days = Math.floor(t / (1000 * 60 * 60 * 24));
 var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((t % (1000 * 60)) / 1000);
+
 document.getElementById("day").innerHTML =days ;
 document.getElementById("hour").innerHTML =hours;
 document.getElementById("minute").innerHTML = minutes;
 document.getElementById("second").innerHTML =seconds;
-if (t < 0) {
+
+    if (t < 0) {
 		clearInterval(x);
 		document.getElementById("demo").innerHTML = "TIME UP";
 		document.getElementById("day").innerHTML ='0';
 		document.getElementById("hour").innerHTML ='0';
 		document.getElementById("minute").innerHTML ='0' ;
-		document.getElementById("second").innerHTML = '0'; }
+		document.getElementById("second").innerHTML = '0'; 
+    }
+
 }, 1000);
 </script>
 </body>
