d0264e3b877ffdb764d934ed3d04b4c9f7194417
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-12 15:38:06 -0700

    up

--- feedback/index.php
+++ feedback/index.php
@@ -1,192 +1,263 @@
 <?php
-require_once '../includes/db.php';
+require_once ('../includes/header.php');
+	require_once '../includes/db.php';
 
-require_once '../Controllers/FeedbackController.php';
+	require_once '../Controllers/CampaignController.php';
 
-$db = new DBController();
+	$db = new DBController();
 
-$conn = $db->connect();
+	$conn = $db->connect();
 
-$dCtrl  =	new FeedbackController($conn);
+	$dCtrl  =	new CampaignController($conn);
 
-$camps = $dCtrl->index();
+	$camps = $dCtrl->index();
+
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <div class="container">
+        <div class="row justify-content-center" style="margin-top: 20px;">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Campaigns
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-outline-primary float-end" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Add
+                        </button>
 
-    <link rel="stylesheet" href="../Vendor/animate.css">
-    <link rel="stylesheet" href="../Vendor/css/bootstrap.css">
-    <link rel="stylesheet" href="../Vendor/css/bootstrap-utilities.min.css">
-    <link rel="stylesheet" href="../Vendor/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../Vendor/css/bootstrap-utilities.css">
-    <link rel="stylesheet" href="../Vendor/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../Vendor/font-awesome/fonts/fontawesome-webfont5b62.eot">
-    <link rel="stylesheet" href="../Vendor/font-awesome/fonts/fontawesome-webfont5b62.svg">
-    <link rel="stylesheet" href="../Vendor/font-awesome/fonts/fontawesome-webfont5b62.ttf">
-    <title>FeedBack</title>
-    <style>
-    .b-example-divider {
-    height: 3rem;
-    background-color: rgba(0, 0, 0, .1);
-    border: solid rgba(0, 0, 0, .15);
-    border-width: 1px 0;
-    box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-    }
-
-    @media (min-width: 992px) {
-    .rounded-lg-3 { border-radius: .3rem; }
-    }
-    </style>
-</head>
-<body>
-<main style="direction: ltr; display: contents; padding: 10%;">
-    <?php foreach ($camps as $camp)
-        echo '
-  
-  <center>
-<div class="card border-info mb-3 animate_animated animate_fadeInRight" style="max-width: 25rem;">
-  <div class="card-header bg-transparent border-success">Feed Back Form</div>
-  <div class="card-body ">
-  <div class="card-img-top"><img src="../review_link/uploads/'.$camp['rev_image'].'" class="img-fluid" alt=""></div>
-  <div id="Intro">
-    <h5 class="card-title">'.$camp['rev_name'].'</h5>
-    <p class="card-text">'.$camp['rev_desc'].'</p>
-    <p class="h3">Would you like to recommend us?</p>
-    <button id="yes" class="btn btn-sm btn-outline-info">Yes</button>
-    <button id="no" class="btn btn-sm btn-outline-warning">No</button>
-  </div>
-  <div id="yesClick" class="visually-hidden">
-    <h5 class="card-title">'.$camp['rev_name'].'</h5>
-    <p class="card-text">'.$camp['rev_pmsg'].'</p>
-  <form action="insert.php" class="text-lg-start" method="post" enctype="multipart/form-data">
-
-    <div class="form-group">
-    <label for="">Review Title</label>
-<input type="text" name="title" id="rev_title" class="form-control">
-</div>   
- <div class="form-group">
-    <label for="">Review Description</label>
-<textarea name="desc" id="rev_desc" class="form-control"></textarea>
-</div>
- <div class="form-group">
-                                                <hr>
-                                                <label for="rev_stars" class="text-center">Star Rating</label>
-                                                <br>
-
-                                                <h4 class="text-center mt-2 mb-4">
-                                                    <i class="fa fa-star star-light submit_star mr-1" id="submit_star_1" data-rating="1"></i>
-                                                    <i class="fa fa-star star-light submit_star mr-1" id="submit_star_2" data-rating="2"></i>
-                                                    <i class="fa fa-star star-light submit_star mr-1" id="submit_star_3" data-rating="3"></i>
-                                                    <i class="fa fa-star star-light submit_star mr-1" id="submit_star_4" data-rating="4"></i>
-                                                    <i class="fa fa-star star-light submit_star mr-1" id="submit_star_5" data-rating="5"></i>
-                                                </h4>
-                                                <input type="hidden" name="rating" id="rating" value="" />
-                                                <input type="hidden" name="user" id="user" value="'.$camp['user_key'].'" />
-                                                <hr>
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="staticBackdropLabel">Add a Campaign</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form id="campInsertForm">
+                                            <div class="form-group">
+                                                <label for="b_name">Campaign Name</label>
+                                                <input type="text" class="form-control" id="c_name" name="c_name" placeholder="Campaign Name" autofocus autocomplete="Name">
                                             </div>
-                                            <div class="form-group d-grid gap-2">
-                                            <button  class="btn btn-primary " id="stepOnesave" style="display: block;">Save</button>
-</div>
+                                            <div class="form-group">
+                                                <label for="b_url">Campaign URL</label>
+                                                <input type="text" class="form-control" id="c_url" name="c_url" placeholder="Campaign URL"  autocomplete="URL">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="c_style">Select Style</label>
+                                                <select name="style" id="c_style" class="form-control">
+                                                    <option value="0">Select Style Please</option>
+                                                    <option value="Rounded">Rounded</option>
+                                                    <option value="Squared">Squared</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="c_delay">Delay</label>
+                                                <input type="text" class="form-control" name="delay" id="c_delay">
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-md-4"> <label for="brd">Branding :</label> <i class="fa fa-toggle-off fa-2x" id="toggle-on"></i>
+                                                </div>
+                                            </div>
+                                            <div class="form-group" id="">
+                                                <select name="branding" id="brandshow" class="form-control">
+                                                    <option value="0"></option>
+                                                </select>
+                                            </div>
+                                            <br>
+                                            <div class="form-group">
+                                                <label for="b_name">Registration Date</label>
+                                                <input type="datetime-local" class="form-control" id="c_date" name="c_date">
+                                            </div>
+                                            <br>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <input type="hidden" id="id" name="bid" />
+                                        <button type="button" class="btn btn-secondary editbrand updatecamp" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary" name="saveBrand" id="saveBrand"> Save</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-</div>
-  </div>
-  <div id="saveClick" class="visually-hidden">
-  <h5 class="card-title">'.$camp['rev_name'].'</h5>
-  <p> <strong>Can we use your Review in our website</strong></p>
-   <button id="yes1" class="btn btn-sm btn-outline-info">Yes</button>
-    <button id="no1" class="btn btn-sm btn-outline-warning" name="Positive_NF">No</button>
-</div>
-<div id="yes_click" class="visually-hidden">
-<div class="form-group">
+                    <div class="card-body table-responsive">
+                        <table class="table table-bordered table-hovered table-striped table-responsive-md" id="productTable">
+                            <thead>
+                            <th>  ID </th>
+                            <th> Name </th>
+                            <th> URL </th>
+                            <th> Style </th>
+                            <th> Delay </th>
+                            <th> branding </th>
+                            <th> Registration Date </th>
+                            <th> Update Date </th>
+                            <th> JS Code </th>
+                            <th> Actions</th>
+                            </thead>
 
-<input type="file" name="file" id="file" class="form-control">
-</div>
-<div class="form-group d-grid gap-2">
-<button class="btn btn-primary" name="Positive_NF">Next</button>
-</div>
-</div>
-<div id="noClick" class="visually-hidden" >
-    <p class="card-text">'.$camp['rev_nmsg1'].'</p>
-        <div class="form-group" style="text-align: left;">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" class="form-control">
-        <input type="hidden" name="user" id="user"  value="'.$camp['user_key'].'" class="form-control">
-</div>
-  <div class="form-group" style="text-align: left;">
-        <label for="phone">Phone (Optional)</label>
-        <input type="phone" name="phone" id="phone" class="form-control">
-        <div class="form-group d-grid gap-2">
-<button class="btn btn-primary" name="Negative_feed">Next</button>
-</div>
-</div>
-</div>
-</form>
-  <div class="card-footer bg-transparent border-info">'.$camp['rev_slug'].'</div>
-</div>
-    </center>
-    
-    ';
+                            <tbody>
+
+                            <?php
+                            foreach($camps as $camp) : ?>
+
+                        
+
+                            
 
 
-        ?>
+                                <tr>
+                                    <td id="b_id"> <?php echo $camp['id']; ?> </td>
+                                    <td> <?php echo $camp['campaign_name']; ?> </td>
+                                    <td> <?php echo $camp['domain_name']; ?> </td>
+                                    <td> <?php echo $camp['selected_style']; ?> </td>
+                                    <td> <?php echo $camp['delay']; ?> </td>
+                                    <td> <?php echo $camp['branding']; ?> </td>
+                                    <td> <?php echo $camp['date_reg']; ?> </td>
+                                    <td> <?php echo $camp['date_update']; ?> </td>
+                                    <td><a href="/campaign/jscode.php" target="_blank">Get Code</a></td>
+                                    <td> <button  id="<?php echo $camp['id'];?>" class="btn btn-info update"><i class="fa fa-edit"></i></button>
+                                        <button  id="<?php echo $camp['id'];?>"  class="btn btn-danger delete"><i class="fa fa-trash"></i></button>
+                                    </td>
+                                </tr>
 
-</main>
-<script src="../Vendor/js/bootstrap.js"></script>
-<script src="../Vendor/js/bootstrap.bundle.js"></script>
-<script src="../Vendor/jquery.js"></script>
-<link rel="stylesheet" href="../Vendor/app.js">
+
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+require_once ('../includes/footer.php');
+?>
 <script>
-    $(document).ready(function () {
-        $('#yes').click(function () {
-            $('#Intro').addClass('visually-hidden');
-            $('#yesClick').removeClass('visually-hidden');
-        });
-        $('#stepOnesave').click(function (e) {
-            e.preventDefault();
-            $('#yesClick').addClass('visually-hidden');
-            $('#saveClick').removeClass('visually-hidden')
+    $(document).ready(function() {
+        $('#productTable').DataTable();
 
-        });
-        $('#yes1').click(function (e) {
-            e.preventDefault();
-            $('#saveClick').addClass('visually-hidden');
-            $('#yes_click').removeClass('visually-hidden');
+        $('#saveBrand').click(function () {
+            var cname = $('#c_name').val();
+            var curl = $('#c_url').val();
+            var cdate = $('#c_date').val();
+            var style = $('#c_style').val();
+            var delay = $('#c_delay').val();
+            var brand = $('#brandshow').val();
 
-        })
-        $(document).on('mouseenter','.submit_star',function () {
-            var rating = $(this).data('rating');
-            reset_background();
-            for(var count =1; count<=rating;count++)
-            {
-                $('#submit_star_'+count).addClass('text-warning');
+            var dataString = "cname1="+cname+ "&curl1="+curl+"&cdate1="+cdate+"&style1="+style+"&delay1="+delay+"&brand1="+brand;
+            try{
+                if(cname =='' || curl =='')
+                {
+                    alert('Please Fill the Fields');
+                }
+                else
+                {
+                    $.ajax({
+                        type:'post',
+                        url:'../campaign/insert.php',
+                        data:dataString,
+                        cache:false,
+                        success:function (data) {
+                            $('#campInsertForm')[0].reset();
+                            $('#staticBackdrop').modal('hide');
+                            alert(data);
+                           // setInterval('refresh()',100);
+                           window.location.reload();
+                        }
+                    })
+                }
+            }
+            catch (e) {
+                alert(e.message);
             }
         });
-        $(document).on('click','.submit_star',function () {
-            rating_data = $(this).data('rating');
-            document.getElementById('rating').value = rating_data;
-            var dd = $('#rating').val();
-        });
-        $('#no').click(function (e) {
-            e.preventDefault();
-            $('#Intro').addClass('visually-hidden'),
-                $('#noClick').removeClass('visually-hidden');
-        });
-        $('$no1').click(function () {
+$(document).on('click','.update',function () {
+    var camp_id = $(this).attr('id');
+    $.ajax({
+        type:'post',
+        url:'../campaign/fetch-single.php',
+        data:{camp_id:camp_id},
+        success:function (data) {
+           $('#staticBackdrop').modal('show');
+           data = jQuery.parseJSON(data);
+           $('#c_name').val(data.campaign_name);
+           $('#c_url').val(data.domain_name);
+           $('#c_date').val(data.date_reg);
+           $('#id').val(data.id);
+           $('#c_style').val(data.style);
+           $('#c_delay').val(data.delay);
+           $('.modal-title').text('Edit Campaign');
+           $('#saveBrand').addClass('disabled btn-warning');
+           $('#saveBrand').attr('type','hidden');
+           $('.editbrand').attr('data-bs-dismiss','');
+           $('.editbrand').text('Update Data');
+           $('.editbrand').attr('id','editcamp')
+           $('#saveBrand').text('Disabled Button');
 
-        })
-    });
-    function reset_background() {
-        for(var count =1;count<=5;count++)
-        {
-            $('#submit_star_'+count).addClass('star-light');
-            $('#submit_star_'+count).removeClass('text-warning');
         }
+    })
+});
+$('.updatecamp').click(function () {
+    var id = $('#id').val();
+    var cname = $('#c_name').val();
+    var curl = $('#c_url').val();
+    var cdate = $('#c_date').val();
+    var style = $('#c_style').val();
+    var delay = $('#c_delay').val();
+    var brand = $('#brandshow').val();
+    var dataString = "cname1="+cname+ "&curl1="+curl+"&cdate1="+cdate+"&style1="+style+"&delay1="+delay+"&brand1="+brand+"&id1="+id;
+   $.ajax({
+       type:"post",
+       url:"update-campaign.php",
+       data:dataString,
+       cache:false,
+       success:function (data) {
+           alert(data);
+           //setInterval('refresh()',100);
+            window.location.reload();
+       }
+
+   })
+});
+$(document).on('click','.delete',function () {
+    var campaign = $(this).attr('id');
+    if(confirm('Are you Sure you want to delete?')) {
+        $.ajax({
+            type: 'post',
+            url: 'delete.php',
+            data: {campaign: campaign},
+            success: function (data) {
+                alert(data);
+                //setInterval('refresh()', 100);
+                 window.location.reload();
+            }
+        });
+    }
+});
+$('#toggle-on').click(function () {
+    if($('#toggle-on').hasClass('fa-toggle-off')) {
+        $('#toggle-on').attr('class', 'fa fa-toggle-on fa-2x');
+        var id = <?php echo $_SESSION['user_key']; ?>;
+        $.ajax({
+            type: 'post',
+            url: 'get-brands.php',
+            data: {id: id},
+            success: function (data) {
+                $('#brandshow').html(data);
+            }
+        });
+    }
+    else
+    {
+        $('#toggle-on').attr('class','fa fa-toggle-off fa-2x');
+        $('#brandshow').html('');
+    }
+});
+    });
+        
+    function refresh() {
+        location.reload(true);
     }
 </script>
-</body>
-</html>
+
