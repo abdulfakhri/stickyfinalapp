19e0d7f34e159cef0501d71901bcd13236495969
Abdul Fakhri <abf.ca@hotmail.com>
2021-10-12 15:04:05 -0700

    up

--- campaign/index.php
+++ campaign/index.php
@@ -163,6 +163,7 @@ require_once ('../includes/footer.php');
                             $('#staticBackdrop').modal('hide');
                             alert(data);
                            // setInterval('refresh()',100);
+                           window.location.reload();
                         }
                     })
                 }
@@ -214,6 +215,7 @@ $('.updatecamp').click(function () {
        success:function (data) {
            alert(data);
            //setInterval('refresh()',100);
+            window.location.reload();
        }
 
    })
@@ -228,6 +230,7 @@ $(document).on('click','.delete',function () {
             success: function (data) {
                 alert(data);
                 //setInterval('refresh()', 100);
+                 window.location.reload();
             }
         });
     }
